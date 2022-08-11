ecf877c3473f253b05624cc640b5c04b0ddb51af
Joanna Krol <joannabkrol@gmail.com>
2022-08-11 12:47:19 +0200

    Add Cards component

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,23 @@
+import Card from "../Card/Card";
+import "./Cards.css";
+
+const Cards = ({ cats }) => {
+  return (
+    <div className="pet-cards-container">
+      {cats.map((cat) => {
+        return (
+          <Card
+            key={cat.id}
+            name={cat.name}
+            phone={cat.phone}
+            email={cat.email}
+            image={cat.image}
+            favoured={cat.favoured}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default Cards;
